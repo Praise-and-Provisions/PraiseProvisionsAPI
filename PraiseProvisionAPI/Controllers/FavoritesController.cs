8fec1697108aed2c7edbb1c9572cfff1fe15c854
jimmyn123 <jimmyn123@gmail.com>
2018-11-06 09:44:36 -0800

    testing continuous deployment

--- PraiseProvisionAPI/Controllers/FavoritesController.cs
+++ PraiseProvisionAPI/Controllers/FavoritesController.cs
@@ -15,7 +15,7 @@ namespace PraiseProvisionsAPI.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Testing Continuous deployment" };
         }
 
         // GET api/<controller>/5
