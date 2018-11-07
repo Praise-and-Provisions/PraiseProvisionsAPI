feebdcc145c27103b49a5256a105a74efd9b9b86
Jimmy Nguyen <jimmyn123@gmail.com>
2018-11-06 21:31:11 -0800

    finished api

--- PraiseProvisionAPI/Controllers/RestaurantController.cs
+++ PraiseProvisionAPI/Controllers/RestaurantController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PraiseProvisionAPI.Models.Interfaces;
 using PraiseProvisionsAPI.Data;
 using PraiseProvisionsAPI.Models;
 
@@ -13,33 +14,34 @@ namespace PraiseProvisionsAPI.Controllers
     [Route("api/[controller]")]
     public class RestaurantController : ControllerBase
     {
-        public PraiseDBContext _context { get; set; }
+        public IRestaurant _context;
 
-        public RestaurantController(PraiseDBContext context)
+        public RestaurantController(IRestaurant context)
         {
             _context = context;
         }
 
         // GET: api/<controller>
         [HttpGet]
-        public ActionResult<IEnumerable<Restaurant>> Get()
+        public IEnumerable<Restaurant> Get()
         {
-            return _context.Restaurants;
+            return _context.GetRestaurants();
         }
 
         // GET api/<controller>/5
         [HttpGet("{id}")]
-        public ActionResult<IActionResult> Get(int id)
+        public async Task<ActionResult<IActionResult>> Get(int id)
         {
-            var restaurant = _context.Restaurants.FirstOrDefault(x => x.ID == id);
-            if (restaurant == null)
+            var chef = await _context.GetRestaurant(id);
+            if (chef == null)
             {
                 return NotFound();
             }
-            return Ok(restaurant);
+            return Ok(chef);
         }
 
         // POST api/<controller>
+        [HttpPost]
         public async Task<IActionResult> Post([FromBody]Restaurant restaurant)
         {
             if (!ModelState.IsValid)
@@ -47,46 +49,34 @@ namespace PraiseProvisionsAPI.Controllers
                 return BadRequest(ModelState);
             }
 
-            await _context.Restaurants.AddAsync(restaurant);
-            await _context.SaveChangesAsync();
+            await _context.CreateRestaurant(restaurant);
 
             return CreatedAtAction("Get", new { id = restaurant.ID }, restaurant);
         }
 
+        //// PUT api/<controller>/5
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Put(int id, [FromBody]Chef chef)
+        //{
 
-        // PUT api/<controller>/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, [FromBody]Restaurant restaurant)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            var found = _context.Restaurants.Where(x => x.ID == id);
+        //    await _context.UpdateChef(id, chef);
 
-            if (found != null)
-            {
-                _context.Restaurants.Update(restaurant);
-            }
-            else
-            {
-                await Post(restaurant);
-            }
-
-            return RedirectToAction("Get", new { id = restaurant.ID });
-        }
+        //    return RedirectToAction("Get", new { id = chef.ID });
+        //}
 
         // DELETE api/<controller>/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            var found = _context.Restaurants.FirstOrDefault(x => x.ID == id);
-
-            if(found != null)
+            var result = await _context.GetRestaurant(id);
+            if (result != null)
             {
-                _context.Restaurants.Remove(found);
-                await _context.SaveChangesAsync();
+                await _context.DeleteRestaurant(result);
             }
 
             return Ok();
